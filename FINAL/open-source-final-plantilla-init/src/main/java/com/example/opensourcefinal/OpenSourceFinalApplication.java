eb4e1b18f9d0df9713f433271c1b6e4d7a3a5aa4
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2018-07-04 23:13:52 -0500

    final template

---
+++ FINAL/open-source-final-plantilla-init/src/main/java/com/example/opensourcefinal/OpenSourceFinalApplication.java
@@ -0,0 +1,12 @@
+package com.example.opensourcefinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSourceFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenSourceFinalApplication.class, args);
+    }
+}
